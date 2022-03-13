ec8c4fdb2509367cf99b65b68dbadcb7c4e5e8e0
acer <mo.bodagh@yahoo.com>
2022-03-13 05:01:03 -0700

    !

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/person")
 public class PersonController
